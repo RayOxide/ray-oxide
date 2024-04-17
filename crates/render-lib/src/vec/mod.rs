3ef1cf30934728d6229a975a4dd42c4d87707197
Jonas Pfeifer <jonaspfeifer@drei.at>
2024-04-17 19:02:31 +0200

    basic vector access functions

--- crates/render-lib/src/vec/mod.rs
+++ crates/render-lib/src/vec/mod.rs
@@ -1,1 +1,21 @@
 pub type Vec3 = [f32; 3];
+
+pub trait Vector {
+    fn x(&self) -> f32;
+    fn y(&self) -> f32;
+    fn z(&self) -> f32;
+}
+
+impl Vector for Vec3 {
+    fn x(&self) -> f32 {
+        self[0]
+    }
+
+    fn y(&self) -> f32 {
+        self[1]
+    }
+
+    fn z(&self) -> f32 {
+        self[2]
+    }
+}
