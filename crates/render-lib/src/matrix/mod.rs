7bb0976c6dc750a2af44bed68521e90f95c611fa
Jonas Pfeifer <jonaspfeifer@drei.at>
2024-04-17 19:16:31 +0200

    added matrix multiplication

--- crates/render-lib/src/matrix/mod.rs
+++ crates/render-lib/src/matrix/mod.rs
@@ -1,1 +1,44 @@
 pub type Matrix3x3 = [[f32; 3]; 3];
+
+pub trait MatrixMul<Rhs> {
+    type Output;
+
+    fn mul(&self, rhs: &Rhs) -> Self::Output;
+}
+
+impl MatrixMul<Matrix3x3> for Matrix3x3 {
+    type Output = Matrix3x3;
+
+    fn mul(&self, rhs: &Matrix3x3) -> Self::Output {
+        [
+            [
+                self[0][0] * rhs[0][0] + self[0][1] * rhs[1][0] + self[0][2] * rhs[2][0],
+                self[0][0] * rhs[0][1] + self[0][1] * rhs[1][1] + self[0][2] * rhs[2][1],
+                self[0][0] * rhs[0][2] + self[0][1] * rhs[1][2] + self[0][2] * rhs[2][2],
+            ],
+            [
+                self[1][0] * rhs[0][0] + self[1][1] * rhs[1][0] + self[1][2] * rhs[2][0],
+                self[1][0] * rhs[0][1] + self[1][1] * rhs[1][1] + self[1][2] * rhs[2][1],
+                self[1][0] * rhs[0][2] + self[1][1] * rhs[1][2] + self[1][2] * rhs[2][2],
+            ],
+            [
+                self[2][0] * rhs[0][0] + self[2][1] * rhs[1][0] + self[2][2] * rhs[2][0],
+                self[2][0] * rhs[0][1] + self[2][1] * rhs[1][1] + self[2][2] * rhs[2][1],
+                self[2][0] * rhs[0][2] + self[2][1] * rhs[1][2] + self[2][2] * rhs[2][2],
+            ],
+        ]
+    }
+}
+
+mod tests {
+
+    #[test]
+    fn mul() {
+        use crate::matrix::{Matrix3x3, MatrixMul};
+        
+        let a: Matrix3x3 = [[1.0,2.0,3.0],[4.0,5.0,6.0],[7.0,8.0,9.0]];
+        let b: Matrix3x3 = [[1.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,1.0]];
+        
+        assert_eq!(a.mul(&b), a)
+    }
+}
